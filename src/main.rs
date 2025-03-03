a29ba615f0badaac6d26496c8bf799247c5e23c0
Mickael <abdealy.mickael@gmail.com>
2025-03-03 10:54:32 +0100

    feat: add download_atp_zip_then_unzip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use regex::Regex;
-use std::error::Error;
 extern crate reqwest;
+extern crate zip;
+
+use core::panic;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::time::Duration;
+
+use regex::Regex;
 
 fn main() {
     let file_url = get_file_url().unwrap();
-    println!("{}", file_url)
+    let output_path = String::from("temp/output.zip");
+    let output_directory = String::from("temp/");
+    download_atp_data(file_url, &output_path).unwrap();
+    unzip(output_path, output_directory)
 }
 
 fn get_file_url() -> Result<String, Box<dyn Error>> {
@@ -21,4 +32,46 @@ fn get_file_url() -> Result<String, Box<dyn Error>> {
         None => return Err("Capture group not found".into()),
     };
     Ok(url.to_string())
+}
+
+fn download_atp_data(url: String, output_path: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new(&output_path);
+    let display = path.display();
+
+    let mut file = match File::create(path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => {
+            println!("File successfully created to {}", display);
+            file
+        }
+    };
+
+    println!("Getting the zip file from {}", url);
+    let client = reqwest::blocking::Client::builder()
+        .timeout(Some(Duration::new(120, 0)))
+        .build()
+        .unwrap();
+
+    let req = client.get(&url).build().unwrap();
+    let resp = client.execute(req)?.bytes()?;
+    println!("Got file from {}", url);
+    match file.write_all(&resp) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("succesfully wrote to {}", display),
+    }
+    Ok(())
+}
+
+fn unzip(path: String, output_directory: String) {
+    let file = File::open(&path).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+    match archive.extract(&output_directory) {
+        Err(why) => {
+            panic!(
+                "couldn't extract {} to {}, error: {}",
+                path, output_directory, why
+            )
+        }
+        Ok(_) => println!("{} successfully extracted to {}", path, output_directory),
+    };
 }
