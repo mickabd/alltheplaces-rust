2d9c629799ef86199c908cb830c90a5f0e30908e
Mickael <abdealy.mickael@gmail.com>
2025-03-12 16:30:48 +0100

    feat: try sqlx to connect to database with async

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::{env, error::Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+    let db_url = env::var("DBURL")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT count(1) FROM poi LIMIT 1")
+        .fetch_one(&pool)
+        .await?;
+
+    println!("row: {}", row.0);
+
+    Ok(())
 }
