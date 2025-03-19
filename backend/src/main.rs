acedaf2640e99ed13525e1094d32f7b58ea4e2d6
Mickael <abdealy.mickael@gmail.com>
2025-03-19 11:14:41 +0100

    feat: add api for brands

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,10 @@
-mod poi;
+mod model;
 use actix_web::{
     App, HttpResponse, HttpServer, Responder, get,
     web::{Data, Path},
 };
 use dotenv::dotenv;
-use poi::Poi;
+use model::{Brand, Poi};
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use std::env;
 
@@ -28,6 +28,8 @@ async fn main() -> Result<(), std::io::Error> {
             .app_data(Data::new(AppState { db: pool.clone() }))
             .service(get_poi_by_id)
             .service(get_random_pois)
+            .service(get_brand_by_id)
+            .service(get_random_brands)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
@@ -52,8 +54,9 @@ async fn get_poi_by_id(state: Data<AppState>, path: Path<i32>) -> impl Responder
 #[get("/poi/random/{count}")]
 async fn get_random_pois(state: Data<AppState>, path: Path<i64>) -> impl Responder {
     let limit = path.into_inner();
+    let max_limit = 15;
     if limit > 15 {
-        return HttpResponse::BadRequest().body("Limit must be less than 10");
+        return HttpResponse::BadRequest().body(format!("Limit must be less than {}", max_limit));
     }
     match sqlx::query_as::<_, Poi>("SELECT * FROM poi LIMIT $1")
         .bind(limit)
@@ -64,5 +67,39 @@ async fn get_random_pois(state: Data<AppState>, path: Path<i64>) -> impl Respond
             HttpResponse::NotFound().body(format!("Error while getting random POIs: {}", why))
         }
         Ok(pois) => HttpResponse::Ok().json(pois),
+    }
+}
+
+#[get("/brand/{id}")]
+async fn get_brand_by_id(state: Data<AppState>, path: Path<i32>) -> impl Responder {
+    let id = path.into_inner();
+    match sqlx::query_as::<_, Brand>("SELECT * FROM brand WHERE id = $1")
+        .bind(id)
+        .fetch_one(&state.db)
+        .await
+    {
+        Err(why) => {
+            HttpResponse::NotFound().body(format!("No poi found with id: {}, error: {}", id, why))
+        }
+        Ok(brand) => HttpResponse::Ok().json(brand),
+    }
+}
+
+#[get("/brand/random/{count}")]
+async fn get_random_brands(state: Data<AppState>, path: Path<i64>) -> impl Responder {
+    let limit: i64 = path.into_inner();
+    let max_limit = 15;
+    if limit > 15 {
+        return HttpResponse::BadRequest().body(format!("Limit must be less than {}", max_limit));
+    }
+    match sqlx::query_as::<_, Brand>("SELECT * FROM brand LIMIT $1")
+        .bind(limit)
+        .fetch_all(&state.db)
+        .await
+    {
+        Err(why) => {
+            HttpResponse::NotFound().body(format!("Error while getting random Brands: {}", why))
+        }
+        Ok(brands) => HttpResponse::Ok().json(brands),
     }
 }
