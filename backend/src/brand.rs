9192184bc5d434d49188525d7d43310e8e552556
Mickael Abdealy <129065216+mickabd@users.noreply.github.com>
2025-03-26 23:07:49 +0100

    feat: use both db for the backend (#35)

--- backend/src/brand.rs
+++ backend/src/brand.rs
@@ -1,4 +1,4 @@
-use crate::{AppState, model::Brand};
+use crate::model::{Brand, DatabaseState};
 use actix_web::{
     HttpResponse, Responder, get,
     web::{Data, Path},
@@ -6,12 +6,12 @@ use actix_web::{
 use log::{debug, error, info};
 
 #[get("/brand/{id}")]
-async fn get_brand_by_id(state: Data<AppState>, path: Path<i32>) -> impl Responder {
+async fn get_brand_by_id(state: Data<DatabaseState>, path: Path<i32>) -> impl Responder {
     let id = path.into_inner();
     debug!("Getting brand with id: {}", id);
     match sqlx::query_as::<_, Brand>("SELECT * FROM brand WHERE id = $1")
         .bind(id)
-        .fetch_one(&state.db)
+        .fetch_one(&state.brand_db)
         .await
     {
         Err(why) => {
@@ -26,10 +26,10 @@ async fn get_brand_by_id(state: Data<AppState>, path: Path<i32>) -> impl Respond
 }
 
 #[get("/brand/random/{count}")]
-async fn get_random_brands(state: Data<AppState>, path: Path<i64>) -> impl Responder {
+async fn get_random_brands(state: Data<DatabaseState>, path: Path<i64>) -> impl Responder {
     let limit: i64 = path.into_inner();
     let max_limit = 15;
-    info!("Request received to get {} random brands", limit);
+    debug!("Request received to get {} random brands", limit);
 
     if limit > max_limit {
         error!("Requested limit {} exceeds max limit {}", limit, max_limit);
@@ -38,7 +38,7 @@ async fn get_random_brands(state: Data<AppState>, path: Path<i64>) -> impl Respo
 
     match sqlx::query_as::<_, Brand>("SELECT * FROM brand LIMIT $1")
         .bind(limit)
-        .fetch_all(&state.db)
+        .fetch_all(&state.brand_db)
         .await
     {
         Err(why) => {
